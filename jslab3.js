82e4988c050b841dec5863ca2db2db24a6180494
Oritsejolomi Sillo <jsillo38@gmail.com>
2020-02-18 14:28:58 +0000

    ‘commit’

---
+++ jslab3.js
@@ -0,0 +1,51 @@
+
+
+function greet(){   
+    return 'Haydo';
+}
+var salutation;
+salutation = greet();
+console.log(salutation);
+
+function echo(sound){
+    return sound;
+}
+
+function greet(name){
+    return 'Hello '+name+' !';
+}
+
+function shout(word){
+    return word+word;
+}
+
+console.log(shout('Fire'));
+
+function doSomething(name){
+    return name;
+}
+doSomething('Eniola');
+
+function multiplyFive(number){
+    return number * 5;
+}
+
+function myFunction(){
+    alert('Hello World');
+}
+myFunction();
+
+var isAnswerRight = true;
+
+isAnswerRight ? console.log('The answer is Right!') : console.log('The answer is Wrong');
+
+function tellFortune(num,name,location,job_title){
+    return 'You will be a '+job+' in'+location+', and married to '+name+' with '+num+' kids.';
+
+}
+
+
+
+
+
+
---
+++ try.js
@@ -0,0 +1,7 @@
+var trying = function(say){
+    return 'hello'+say;
+};
+
+var x = trying('bye');
+
+console.log(x);
